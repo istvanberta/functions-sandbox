52a3e87bc03352fc1c456428200245a50c895041
István Berta <istvan.berta@gmail.com>
2021-04-01 22:34:05 +0200

    Implement label editing.

--- multi-variable/multi-variable.js
+++ multi-variable/multi-variable.js
@@ -3,6 +3,9 @@ const calculator = document.forms.calculator;
 let numberOfInputs = 2;
 let y = function() {return 0};
 
+
+// updateOutput() functionality
+
 function updateOutput() {
     calculator.output.value = y(...getInputValues());
 }
@@ -14,6 +17,9 @@ function getInputValues() {
 function selectInputs() {
     return calculator.querySelectorAll('#inputs input');
 }
+
+
+// addInput() functionality
 
 function addInput() {
     let newInput = calculator.querySelector('#inputs>.input').cloneNode(true);
@@ -29,5 +35,30 @@ function setNextInputNumber(input) {
     return input;
 }
 
+
+// toggleEditor() functionality
+
+function toggleEditor() {
+    calculator.querySelectorAll('.label-text-editor').forEach(e => e.hidden = !e.hidden);
+    calculator.querySelectorAll('.editable').forEach(e => e.hidden = !e.hidden);
+    calculator.querySelector('#save-labels-btn').toggleAttribute('hidden');
+}
+
+function saveLabels() {
+    let labelTexts = getLabelTexts();
+    calculator.querySelectorAll('.label-text').forEach( (e, i) => e.innerText = labelTexts[i]);
+    toggleEditor();
+}
+
+function getLabelTexts() {
+    return [...selectLabelTexts()].map(input => input.value);
+}
+
+function selectLabelTexts() {
+    return calculator.querySelectorAll('.label-text-editor');
+}
+
 calculator.addEventListener('input', updateOutput);
 calculator.querySelector('#add-input-btn').addEventListener('click', addInput);
+calculator.querySelector('#edit-labels-btn').addEventListener('click', toggleEditor); // #toggle-label-editor
+calculator.querySelector('#save-labels-btn').addEventListener('click', saveLabels);
